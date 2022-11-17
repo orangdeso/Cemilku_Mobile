77ccae1a4ab83067dce75a687464908c28ca7924
orangdeso <100177534+orangdeso@users.noreply.github.com>
2022-11-17 15:25:01 +0700

    Update1 From jaki E41212036

---
+++ app/src/main/java/com/example/cemilku/Model/DataItem.java
@@ -0,0 +1,47 @@
+package com.example.cemilku.Model;
+
+public class DataItem {
+    private String email;
+    private String nama_user;
+    private String telpon;
+    private String password;
+
+    public DataItem(String email, String nama_user, String telpon, String password){
+        this.email = email;
+        this.nama_user = nama_user;
+        this.telpon = telpon;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNama_user() {
+        return nama_user;
+    }
+
+    public void setNama_user(String nama_user) {
+        this.nama_user = nama_user;
+    }
+
+    public String getTelpon() {
+        return telpon;
+    }
+
+    public void setTelpon(String telpon) {
+        this.telpon = telpon;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
