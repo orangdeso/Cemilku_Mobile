76684ad4dce3dbcb0db48f8ebe17ae858e3e0a35
orangdeso <100177534+orangdeso@users.noreply.github.com>
2022-11-17 15:19:14 +0700

    Update1 From Jaki E41212036

--- app/src/main/java/com/example/cemilku/API/APIRequestData.java
+++ app/src/main/java/com/example/cemilku/API/APIRequestData.java
@@ -1,11 +1,25 @@
-//package com.example.cemilku.API;
-//
-//import com.example.cemilku.Model.ResponseModel;
-//
-//import retrofit2.Call;
-//import retrofit2.http.GET;
-//
-//public interface APIRequestData {
-//    @GET("retrieve.php")
-//    Call<ResponseModel> ardRetrieveData();
-//}
+package com.example.cemilku.API;
+
+import com.example.cemilku.Model.ResponseModel;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface APIRequestData {
+    @FormUrlEncoded
+    @POST("retrieve.php")
+    Call<ResponseModel> loginResponse(
+            @Field("email") String email,
+            @Field("password") String pass
+    );
+//    @FormUrlEncoded
+//    @POST("register.php")
+//    Call<Register> registerResponse(
+//            @Field("nama") String nama,
+//            @Field("email") String email,
+//            @Field("pass") String pass
+//    );
+
+}
--- app/src/main/java/com/example/cemilku/API/RetroServer.java
+++ app/src/main/java/com/example/cemilku/API/RetroServer.java
@@ -1,19 +1,19 @@
-//package com.example.cemilku.API;
-//
-//import retrofit2.Retrofit;
-//import retrofit2.converter.gson.GsonConverterFactory;
-//
-//public class RetroServer {
-//    private static final String baseURL = "http://10.0.2.2/Cemilku/";
-//    private static Retrofit retro;
-//
-//    public static Retrofit konekRetrofit(){
-//        if(retro == null) {
-//            retro = new Retrofit.Builder()
-//                    .baseUrl(baseURL)
-//                    .addConverterFactory(GsonConverterFactory.create())
-//                    .build();
-//        }
-//        return retro;
-//    }
-//}
+package com.example.cemilku.API;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetroServer {
+    private static final String baseURL = "http://192.168.137.57/Php Vs Code/Cemilku/";
+    private static Retrofit retro;
+
+    public static Retrofit getClient(){
+        if(retro == null) {
+            retro = new Retrofit.Builder()
+                    .baseUrl(baseURL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retro;
+    }
+}
