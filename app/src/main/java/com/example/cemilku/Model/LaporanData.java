f6e9c9036d64eca511295e1fc3844925761868b1
rakharn <rakhaarian1@gmail.com>
2022-12-13 10:53:38 +0700

    Update Rakha E412122141

--- app/src/main/java/com/example/cemilku/Model/LaporanData.java
+++ app/src/main/java/com/example/cemilku/Model/LaporanData.java
@@ -1,50 +1,61 @@
 package com.example.cemilku.Model;
 
-public class LaporanData {
+import com.google.gson.annotations.SerializedName;
 
-    private String id_orders;
-    private String placed_on;
-    private String total_products;
-    private String total_price;
-    private String nama_user;
+public class LaporanData{
 
-    public String getId_orders() {
-        return id_orders;
+    @SerializedName("total_price")
+    private String totalPrice;
+
+    @SerializedName("qty")
+    private String qty;
+
+    @SerializedName("placed_on")
+    private String placedOn;
+
+    @SerializedName("nama_user")
+    private String namaUser;
+
+    @SerializedName("id_orders")
+    private String idOrders;
+
+    public void setTotalPrice(String totalPrice){
+        this.totalPrice = totalPrice;
     }
 
-    public void setId_orders(String id_orders) {
-        this.id_orders = id_orders;
+    public String getTotalPrice(){
+        return totalPrice;
     }
 
-    public String getPlaced_on() {
-        return placed_on;
+    public void setQty(String qty){
+        this.qty = qty;
     }
 
-    public void setPlaced_on(String placed_on) {
-        this.placed_on = placed_on;
+    public String getQty(){
+        return qty;
     }
 
-    public String getTotal_products() {
-        return total_products;
+    public void setPlacedOn(String placedOn){
+        this.placedOn = placedOn;
     }
 
-    public void setTotal_products(String total_products) {
-        this.total_products = total_products;
+    public String getPlacedOn(){
+        return placedOn;
     }
 
-    public String getTotal_price() {
-        return total_price;
+    public void setNamaUser(String namaUser){
+        this.namaUser = namaUser;
     }
 
-    public void setTotal_price(String total_price) {
-        this.total_price = total_price;
+    public String getNamaUser(){
+        return namaUser;
     }
 
-    public String getNama_user() {
-        return nama_user;
+    public void setIdOrders(String idOrders){
+        this.idOrders = idOrders;
     }
 
-    public void setNama_user(String nama_user) {
-        this.nama_user = nama_user;
+    public String getIdOrders(){
+        return idOrders;
     }
 }
